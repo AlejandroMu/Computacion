65da5b6168f8a7d07ff68975b56398eb8d6bfcde
alejandroMu <alejandrobravo0329@gmail.com>
2019-04-06 17:13:11 -0500

    ultima edicion

--- copia/inyeccionAnotacion/no-injection/src/main/java/co/edu/icesi/ci/injectionexample1/service/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package co.edu.icesi.ci.injectionexample1.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface RegistrationService {
-
-	public boolean enrolStudent(String studentId, String courseId, int semester);
-}
